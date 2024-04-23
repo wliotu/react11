5944694bc1eeeacb493dc7182972bb90de5448bb
selima <musaevas203@gmail.com>
2024-04-23 17:20:11 +0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header'
+import Home from './components/Home/Home.jsx'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Home />
     </div>
   );
 }
 
 export default App;
+
